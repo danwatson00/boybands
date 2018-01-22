e71197d27f7de2b028b88370226784c6572c38d5
Dan Watson <danwatson00@yahoo.com>
2018-01-22 10:43:22 -0600

    update

--- boybands.js
+++ boybands.js
@@ -1,2 +1,22 @@
 let bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 let vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+// Get a reference to the appropriate DOM element for bands
+const bandElement = document.getElementById(boy-bands);
+
+// Get a reference to the appropriate DOM element for vegetables
+const veggieElement = document.getElementById(vegetables);
+
+// Execute a for loop that will iterate over the arrays
+for (let loopTracker = 0; loopTracker < 4; loopTracker += 1) {
+
+    // Get a reference to the current item in the bands array
+    const currentBand = document(loopTracker);
+
+    // Update the innerHTML value of the DOM element for bands
+document.getElementById(boy-bands).innerHTML;
+    // Get a reference to the current item in the vegetables array
+    const currentVeggie = ???;
+
+    // Update the innerHTML value of the DOM element for vegetables
+}hini", "Broccoli", "Squash"];
